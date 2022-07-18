b11ec3af7cd523a46eab92cc8638b72b2e29e6e0
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-07-18 21:44:02 +0900

    feat: import user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { HelloModule } from './shared/modules/hello/hello.module';
+import { UserModule } from './shared/modules/user/user.module';
 
 @Module({
   imports: [
     HelloModule,
+    UserModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [`.env`],
