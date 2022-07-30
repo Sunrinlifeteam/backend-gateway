5f42ae3f8f1dd8802e0f222eaf229626825173b6
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-07-31 01:46:55 +0900

    feat: move modules folder from shared

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { HelloModule } from '../modules/hello/hello.module';
-import { UploadModule } from '../modules/upload/upload.module';
-import { UserModule } from '../modules/user/user.module';
+import { HelloModule } from './modules/hello/hello.module';
+import { UploadModule } from './modules/upload/upload.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
