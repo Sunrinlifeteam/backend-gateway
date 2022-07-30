7a0f9602c80e5c66d9e4e521e93e2afbdbe9579b
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-07-31 01:58:10 +0900

    fix: upload controller

--- src/modules/upload/upload.controller.ts
+++ src/modules/upload/upload.controller.ts
@@ -1,18 +1,16 @@
 import {
   Controller,
-  Get,
   OnModuleInit,
-  Param,
+  Post,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { Client, ClientGrpc } from '@nestjs/microservices';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Observable } from 'rxjs';
-import { grpcClientOptions } from 'src/shared/options/user.grpc';
+import { grpcClientOptions } from 'src/shared/options/upload.grpc';
 import { UploadService } from 'src/shared/services/upload.service';
 import { FileResponse } from 'src/shared/transfer/upload.dto';
-import { User } from 'src/shared/transfer/user.dto';
 
 @Controller('upload')
 export class UploadController implements OnModuleInit {
@@ -20,14 +18,15 @@ export class UploadController implements OnModuleInit {
   private userService: UploadService;
 
   onModuleInit() {
-    this.userService = this.client.getService<UploadService>('UserService');
+    this.userService = this.client.getService<UploadService>('UploadService');
   }
 
-  @Get('/image')
+  @Post('/image')
   @UseInterceptors(FileInterceptor('image'))
   uploadImage(
     @UploadedFile() imageFile: Express.Multer.File,
-  ): Observable<FileResponse> {
+  ): string | Observable<FileResponse> {
+    if (!imageFile) return 'File is not provided';
     return this.userService.uploadImage(imageFile);
   }
 }
