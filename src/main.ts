09bc8a421e4e43cc45c55bf6c946335c0aa21d76
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-08-09 10:59:41 +0900

    feat: create document

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,35 @@
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+async function swagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Sunrinlife REST API Gateway')
+    .setDescription('Suninlife project REST API description')
+    .setVersion('1.0')
+    .addTag('REST')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('document/rest', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const config = app.get(ConfigService);
+
+  if (config.get<string>('NODE_ENV') === 'development') {
+    await swagger(app);
+  }
+
   await app.listen(config.get<number>('PORT'));
+
+  Logger.log(
+    `Server running on ${config.get<string>('NODE_ENV')} mode`,
+    'Bootstrap',
+  );
   Logger.log(
     `Server running on http://localhost:${config.get<number>('PORT')}`,
     'Bootstrap',
