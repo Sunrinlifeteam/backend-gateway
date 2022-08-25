266f12ef2035f38678c479acc229d99ff9b585d3
rmagur1203 <rmagur1203@naver.com>
2022-08-25 10:33:46 +0900

    feat: processing guard error

--- src/modules/auth/guards/access.guard.ts
+++ src/modules/auth/guards/access.guard.ts
@@ -1,5 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class AccessGuard extends AuthGuard('access') {}
+export class AccessGuard extends AuthGuard('access') {
+  handleRequest(err, user, info) {
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
+}
